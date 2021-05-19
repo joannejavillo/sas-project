5c8dfa464e1ae8f7fb0dfcb74daa90f09e2a93e8
Joan J <joanne.javillo@gmail.com>
2021-05-19 17:59:10 +0930

    added POST routes for the drug and supplier

--- routes/api/drug-routes.js
+++ routes/api/drug-routes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const { Supplier, Drug } = require('../../models');
 
 // The `/api/categories` endpoint
-// find all categories
-  // be sure to include its associated Products
+// find all drugs
+  // be sure to include its associated suppliers
 
   router.get('/', async (req, res) => {  
   try {
@@ -15,5 +15,15 @@ const { Supplier, Drug } = require('../../models');
     res.status(500).json(err);
   }
 });
+
+  // create a new drug
+  router.post('/', async (req, res) => {
+    try {
+      const drugData = await Drug.create(req.body);
+      res.status(200).json(drugData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
 module.exports = router;
--- routes/api/supplier-routes.js
+++ routes/api/supplier-routes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const { Drug, Supplier } = require('../../models');
 
 // The `/api/categories` endpoint
-// find all categories
-  // be sure to include its associated Products
+// find all supplier
+  // be sure to include its associated drugs
 
   router.get('/', async (req, res) => {  
   try {
@@ -13,6 +13,16 @@ const { Drug, Supplier } = require('../../models');
     res.status(200).json(supplierData);
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+// create a new supplier
+router.post('/', async (req, res) => {
+  try {
+    const supplierData = await Supplier.create(req.body);
+    res.status(200).json(supplierData);
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
 
