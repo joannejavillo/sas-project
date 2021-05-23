f005c063c0a1628df14c246f184a5bea714b66b4
Joan J <joanne.javillo@gmail.com>
2021-05-23 17:50:33 +0930

    changed and installed to semantic ui-react dependency

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router } from "react-router-dom";
+//import logo from "./logo.svg";
 import "./App.css";
+import Appbar from "./components/Appbar";
+//import { Navbar } from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+
+  
+
+    <Router>
+
+      <Appbar/> 
+
+    </Router>
   );
 }
 
