516444183b8d4a616c1ac5a751f94b2cb0aecd2b
Joan J <joanne.javillo@gmail.com>
2021-05-30 01:33:08 +0930

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize (process.env>JAWSDB_URL); 
+  sequelize = new Sequelize (process.env.JAWSDB_URL); 
 } else {
-  sequelize = new Sequeliza(
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PW,
